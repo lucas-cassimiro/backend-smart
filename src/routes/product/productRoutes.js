d1d3a0d6baf75bd2f7cd7ad6ff4811560ffebba5
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-10-05 20:20:55 -0300

    product routs with post and get method

--- src/routes/product/productRoutes.js
+++ src/routes/product/productRoutes.js
@@ -1,4 +1,34 @@
 const express = require('express')
 const router = express.Router()
+const model = require('./../../database/models/productModel')
+
+router.get('/', async(req,res) => {
+
+    try {
+
+        const teste = await model.find({})
+        res.send(teste)
+
+    } catch (error) {
+        console.log(error)
+    }
+
+})
+
+router.post('/', async(req, res)=>{
+
+    try {
+        
+        console.log(req.body)
+
+        const createProduct = await model.create(req.body)
+
+        res.send(createProduct) 
+
+    } catch (error) {
+        
+    }
+
+})
 
 module.exports = router
